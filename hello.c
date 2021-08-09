c6dc636177533a8a94796a2ca8418afba1f60f5d
chrisatgitnow <42006085+chrisatgitnow@users.noreply.github.com>
2021-08-09 10:26:29 +0200

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+     int a = get_int("Type in first input number:");
+     int b = get_int("Type in second input number:");
+       /*do while(isspace(*a)) a++; while(*b++ = *a++);
+*/
+      int c = a + b;
+
+    printf("The result is: %d", c);
+}
